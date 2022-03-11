0121765552b08e0fd69054b35b138e52cb96cd2f
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2022-03-11 18:14:34 +0000

    universal date

--- c/test_exceptions.c
+++ c/test_exceptions.c
@@ -2,9 +2,10 @@
 #define USE_EXCEPTION_MAIN
 
 #include "exceptlib.h"
-#include "assert.h"
+#include <assert.h>
 
-int div(int a, int b) {
+
+int div2(int a, int b) {
   printf("Divide %d by %d\n", a, b);
   if ( b == 0 ) {
       RAISE(DIVIDE_BY_ZERO);
@@ -18,7 +19,7 @@ void test_embedded_try_catch()
 {
   TRY
     TRY
-      div(2,0);
+      div2(2,0);
       assert(1==0);             // we assert here as we should not reach it
     CASE DIVIDE_BY_ZERO:
       exceptlib_trace();
@@ -34,7 +35,7 @@ void test_embedded_try_catch()
 void test_single_try_catch() 
 {
   TRY
-    div(2,0);
+    div2(2,0);
     assert(1==0);             // we assert here as we should not reach it
   CASE DIVIDE_BY_ZERO:
     assert(_EXCEPTION_LIST->type == DIVIDE_BY_ZERO);
